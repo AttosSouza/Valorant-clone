ae35b47d9f93eb0bd3992dc6599a2f60d3d11bb1
Atos Souza <attos.souza18@gmail.com>
2024-01-10 21:47:09 -0300

    Switching the <image> tag to component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import ValorantLetter from '../../assets/images/logo-valorant-letter.svg?react';
 
 const Hero = () => {
   return (
@@ -7,10 +8,7 @@ const Hero = () => {
       <div className="container hero">
         <div className="hero-content">
           <h1>Um jogo de tiro tático baseado em personagem 5v5</h1>
-          <img
-            src="/src/assets/images/logo-valorant-letter.svg"
-            alt="Valorant"
-          />
+          <ValorantLetter />
           <p>
             Valorant é um jogo eletrônico multijogador gratuito para jogar de
             tiro
